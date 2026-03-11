e9056ba69d4673980606b22d640cbe4d4aab3ba6
fierzco <fierzcofierz@gmail.com>
2026-03-10 20:04:27 -0400

    Replace ResourceLocation with Identifier in GameMode

--- src/main/java/com/kevin/tiertagger/model/GameMode.java
+++ src/main/java/com/kevin/tiertagger/model/GameMode.java
@@ -1,7 +1,7 @@
 package com.kevin.tiertagger.model;
 
-import net.minecraft.resources.ResourceLocation;
 import it.unimi.dsi.fastutil.Pair;
+import net.minecraft.util.Identifier;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextColor;
@@ -44,7 +44,7 @@ public static CompletableFuture<List<GameMode>> fetchGamemodes(HttpClient client
             Component icon = Component.literal(String.valueOf(pair.left()))
                     .withStyle(s -> s
                             .withColor(pair.right())
-                            .withFont(new ResourceLocation("tier-tagger", "icons")));
+                            .withFont(Identifier.of("tier-tagger", "icons")));
 
             Component name = Component.literal(" " + this.title)
                     .withStyle(s -> s.withColor(pair.right()));
